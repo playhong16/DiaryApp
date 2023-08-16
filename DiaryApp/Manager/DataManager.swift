4c3fe4b85e3c183233fc8fe5725ddb455f905ae3
Hongsik Choi <playhong16@gmail.com>
2023-08-16 09:43:42 +0900

    DataManager 추가

---
+++ DiaryApp/Manager/DataManager.swift
@@ -0,0 +1,37 @@
+//
+//  DataManager.swift
+//  DiaryApp
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import Foundation
+
+final class DataManager {
+    static let shared = DataManager()
+    private init() { }
+    
+    private var DiaryList: [Diary] = []
+    
+    func saveDiary(data: Diary) {
+        DiaryList.append(data)
+    }
+    
+    func getDiary() -> [Diary] {
+        return DiaryList
+    }
+    
+    func updateDiary(data: Diary) {
+        let index = DiaryList.firstIndex { $0.id == data.id }
+        if let index = index {
+            DiaryList[index] = data
+        }
+    }
+    
+    func removeDiary(id: UUID) {
+        let index = DiaryList.firstIndex { $0.id == id }
+        if let index = index {
+            DiaryList.remove(at: index)
+        }
+    }
+}
