9af4009b7ff13323b72a8223acc053b1ee8008df
Hongsik Choi <playhong16@gmail.com>
2023-08-16 10:33:44 +0900

    [FEAT]: TabBar 및 HomePage 초기 세팅

---
+++ HomePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageViewController.swift
+//  DiaryApp
+//
+//  Created by playhong on 2023/08/16.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ TabBarViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.swift
+//  DiaryApp
+//
+//  Created by playhong on 2023/08/16.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+}
+
