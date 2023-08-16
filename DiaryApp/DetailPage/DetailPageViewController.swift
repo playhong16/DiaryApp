b18768d3558900e55b29c81b53c891ec5912a21b
kimhr <khl9566@naver.com>
2023-08-17 01:41:39 +0900

    230817 UI 작업

---
+++ DiaryApp/DetailPage/DetailPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailPageViewController.swift
+//  DiaryApp
+//
+//  Created by t2023-m0067 on 2023/08/16.
+//
+
+import UIKit
+
+class DetailPageViewController: UIViewController {
+
+//        func instantiateViewController(withIdentifier: "DetailPageController") -> UIViewController
+    
+    // 뒤로가기
+    @IBAction func BackBTN(_ sender: UIBarButtonItem) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+  
+    
+
+}
